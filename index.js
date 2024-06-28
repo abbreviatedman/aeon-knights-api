acf66ca64b542bfe9096008d3a7fa42d1187e174
Colin Jaffe <colin.jaffe@gmail.com>
2024-06-28 00:39:17 -0400

    Separates cards from physical cards.

--- index.js
+++ index.js
@@ -3,40 +3,48 @@ const fs = require("fs");
 
 const readJson = async () => {
   const cards = await csv().fromFile("./cards.csv");
-  const json = JSON.stringify(
-    cards.slice(1).map((card) => {
-      const newCard = {
-        ...card,
-        value: Number(card.value),
-        release: Number(card.release),
-      };
+  const cleanedCards = cards.slice(1).map((card) => {
+    const newCard = {
+      ...card,
+      value: Number(card.value),
+      release: Number(card.release),
+    };
 
-      for (const key of ["boxCode", "cardNum", "box"]) {
-        delete newCard[key];
-      }
-
-      for (const key of Object.keys(card)) {
-        if (newCard[key] === "") {
-          delete newCard[key];
-        }
-      }
-
-      return newCard;
-    }),
-
-    null,
-    2
-  );
-
-  fs.writeFile("./cards.json", json, (err) => {
-    if (err) {
-      console.log(err);
-
-      return;
+    for (const key of ["boxCode", "cardNum", "box"]) {
+      delete newCard[key];
     }
 
-    console.log("Success!");
+    for (const key of Object.keys(card)) {
+      if (newCard[key] === "") {
+        delete newCard[key];
+      }
+    }
+
+    return newCard;
   });
+
+  const noDupes = cleanedCards.filter(
+    (card, i) =>
+      i === cleanedCards.length - 1 ||
+      card.name !== cleanedCards[i + 1].name ||
+      card.release !== cleanedCards[i + 1].release
+  );
+
+  fs.writeFile("./cards.json", JSON.stringify(noDupes, null, 2), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  fs.writeFile(
+    "./physical-cards.json",
+    JSON.stringify(cleanedCards, null, 2),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 };
 
 readJson();
